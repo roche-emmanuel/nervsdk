5b36e75fc655fab83c9124dd47673dacf17390d0
administrator <roche.emmanuel@gmail.com>
2025-12-31 16:17:48 +0100

    Removed stb implementation from nervsdk

--- sources/nvk_common.cpp
+++ sources/nvk_common.cpp
@@ -1,14 +1,17 @@
 // precomp.h auto included:
 // #include <nvk_precomp.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include <external/stb/stb_image.h>
+// Note we cannot include this here as this will conflict with Unreal engine
+// libraries:
 
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include <external/stb/stb_image_resize.h>
+// #define STB_IMAGE_IMPLEMENTATION #include
+// <external/stb/stb_image.h>
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <external/stb/stb_image_write.h>
+// #define STB_IMAGE_RESIZE_IMPLEMENTATION
+// #include <external/stb/stb_image_resize.h>
+
+// #define STB_IMAGE_WRITE_IMPLEMENTATION
+// #include <external/stb/stb_image_write.h>
 
 // On emscripten we only have support for 32bit target for now.
 #ifndef __EMSCRIPTEN__
