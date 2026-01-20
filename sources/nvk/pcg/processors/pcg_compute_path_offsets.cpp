9b4c6acd6a48efef5004b618d27eac9cbf84c10d
administrator <roche.emmanuel@gmail.com>
2026-01-20 22:36:46 +0100

    Updated compute_path_offsets

--- sources/nvk/pcg/processors/pcg_compute_path_offsets.cpp
+++ sources/nvk/pcg/processors/pcg_compute_path_offsets.cpp
@@ -14,10 +14,13 @@ void pcg_compute_path_offsets(PCGContext& ctx) {
     PointArrayVector& arrays = in.get("In");
 
     F64 distance = in.get("Distance");
-    NVCHK(distance > 0.0, "Invalid distance.");
+    NVCHK(distance != 0.0, "Invalid distance.");
 
     // Process all paths together:
     Clipper2Lib::PathsD cPaths;
+    cPaths.reserve(arrays.size());
+
+    I32 closed = -1;
 
     for (const auto& path : arrays) {
         U32 num = path->get_num_points();
@@ -26,47 +29,51 @@ void pcg_compute_path_offsets(PCGContext& ctx) {
         }
 
         Clipper2Lib::PathD cPath;
-        cPath.resize(path->is_closed_loop() ? num + 1 : num);
+        cPath.resize(num);
 
-        for (I32 i = 0; i < num; ++i) {
+        I32 newClosed = path->is_closed_loop() ? 1 : 0;
+        if (closed == -1) {
+            closed = newClosed;
+        } else {
+            NVCHK(closed == newClosed,
+                  "pcg_compute_path_offsets: Mixing closed/non closed paths.");
+        }
+
+        for (U32 i = 0; i < num; ++i) {
             auto pos = path->get_point(i).position().xy();
             cPath[i] = {pos.x(), pos.y()};
         }
 
-        if (path->is_closed_loop()) {
-            auto pos = path->get_point(0).position().xy();
-            cPath[num] = {pos.x(), pos.y()};
-        }
         cPaths.emplace_back(std::move(cPath));
+    }
+
+    PointArrayVector contours;
+
+    if (closed == -1) {
+        // No output to provide:
+        ctx.outputs().set("Out", contours);
+        return;
     }
 
     Clipper2Lib::PathsD solution = Clipper2Lib::InflatePaths(
         cPaths, distance, Clipper2Lib::JoinType::Round,
-        Clipper2Lib::EndType::Round);
+        closed == 1 ? Clipper2Lib::EndType::Polygon
+                    : Clipper2Lib::EndType::Round);
 
-    PointArrayVector contours;
     for (const auto& sol : solution) {
         if (sol.empty()) {
             continue;
         }
 
         RefPtr<PointArray> contour = PointArray::create((I32)sol.size());
-        U32 ptSize = sizeof(Clipper2Lib::PointD);
         auto& positions = contour->add_attribute<Vec3d>(pt_position_attr);
 
-        if (ptSize == sizeof(Vec2d)) {
-            // Copy the 2D point data:
-            for (U32 i = 0; i < sol.size(); ++i) {
-                positions[i].set(sol[i].x, sol[i].y, 0.0);
-            }
-        } else if (ptSize == sizeof(Vec3d)) {
-            auto& positions = contour->add_attribute<Vec3d>(pt_position_attr);
-            // Copy the 2D point data:
-            memcpy(positions.data(), sol.data(), sol.size() * ptSize);
+        // Copy the 2D point data:
+        for (U32 i = 0; i < sol.size(); ++i) {
+            positions[i].set(sol[i].x, sol[i].y, 0.0);
         }
 
-        // Setting this contour as closed loop (but not sure this is a good
-        // idea actually ?)
+        // Setting this contour as closed loop:
         contour->set_closed_loop(true);
 
         contours.emplace_back(contour);
