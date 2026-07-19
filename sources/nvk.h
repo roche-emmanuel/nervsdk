b58699d30f6d47223a191c2d96a1c5b7a277b45b
manu <roche.emmanuel@gmail.com>
2026-07-19 05:52:39 +0200

    Adding tracy support

--- sources/nvk.h
+++ sources/nvk.h
@@ -1,5 +1,18 @@
 #ifndef NV_NVK
 
+#ifdef NV_TRACY_ENABLE
+#include <tracy/Tracy.hpp>
+#define TRACY_ZONE() ZoneScoped
+#define TRACY_ZONE_N(name) ZoneScopedN(name)
+#else
+#define TRACY_ZONE()                                                           \
+    do {                                                                       \
+    } while (0)
+#define TRACY_ZONE_N(name)                                                     \
+    do {                                                                       \
+    } while (0)
+#endif
+
 #include <nvk_common.h>
 
 #include <nvk/utils.h>
